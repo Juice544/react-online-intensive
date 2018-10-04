3635bd09ba46e1c61e395f5e9205702a2761a403
Juice544 <vereshagin_sergey@mail.ru>
2018-10-04 23:40:07 +0300

    made lessno 6

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import PropTypes from 'prop-types';
+import { func, string, array, number } from 'prop-types';
+
+import Like from 'components/Like';
+import { Consumer } from 'components/HOC/withProfile';
 
 import Styles from './styles.m.css';
-import { Consumer } from 'components/HOC/withProfile';
 
 export default class Post extends Component {
     static propTypes = {
-        comment: PropTypes.string.isRequired,
-        created: PropTypes.number.isRequired,
+        _removePost: func.isRequired,
+        _likePost: func.isRequired,
+        comment: string.isRequired,
+        created: number.isRequired,
+        id: string.isRequired,
+        likes: array.isRequired,
+    }
+    constructor(){
+        super();
+
+        this._removePost = this._removePost.bind(this);
+    }
+    _removePost () {
+        const { _removePost, id } = this.props;
+
+        _removePost(id);
     }
     render () {
-        const { comment, created } = this.props;
+        const { comment, created, _likePost, id, likes } = this.props;
         return (
         <Consumer>
             {(context) => (
-                <section className = {Styles.post}> 
+                <section className = {Styles.post}>
+                    <span className = {Styles.cross} id = { id } onClick = { this._removePost } ></span> 
                     <img src = {context.avatar} />
                     <a>{context.currentUserFirstName} {context.currentUserLastName}</a>
                     <time>
                         {moment.unix(created).format('MMMM DD h:mm:ss a')}
                     </time>
                     <p>{comment}</p>
+                    <Like _likePost = { _likePost } id = { id } likes = { likes } { ...context } />
                 </section>
             )}
         </Consumer>
